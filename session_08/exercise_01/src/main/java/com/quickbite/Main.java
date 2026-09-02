1f38282595bf5122ea8b1d93f57fe36ed3e90559
Khac-Hai <haikhac23122004@gmail.com>
2026-09-03 06:15:17 +0700

    up ss8 ex1

---
+++ session_08/exercise_01/src/main/java/com/quickbite/Main.java
@@ -0,0 +1,10 @@
+package com.quickbite;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("==================================================");
+        System.out.println("  QuickBite Java Service - Docker Build Test OK!  ");
+        System.out.println("  Docker-outside-of-Docker (DooD) Working Properly");
+        System.out.println("==================================================");
+    }
+}
